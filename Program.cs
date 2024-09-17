cbffc5c47d98e4f30cea7df059d220c5bbcf19c3
Ethan Muller <eman3001@yahoo.com>
2024-09-17 18:10:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("Greetings, Human.");
+Console.WriteLine("What might your name be?");
+string name = Console.ReadLine();
+Console.WriteLine("Your name is....");
+Console.Write(name);
+Console.Write("?");
+Console.WriteLine(" ");
+Console.WriteLine("That's a dumb name");
+
+Console.WriteLine("Anyways I have a cool set of things for you to choose between!");
+Console.WriteLine("Wanna see them? Yes or no?");
+String Answer1 = Console.ReadLine();
+if (Answer1 != ("yes"))
+    Console.WriteLine("Wonderful!");
+else Console.WriteLine("Too bad, stupid.");
